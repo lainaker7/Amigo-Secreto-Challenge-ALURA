73e48c69fee8b6be4c3e64a2821c92b28758495e
Lainaker Fernandez <lainaker7@gmail.com>
2025-08-19 17:53:56 -0400

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
-// primero definimos las variables para el juego del amigo secreto
+let listaDeAmigos =[];
 
-let amigos = [];
-let listaDeParticipantes = document.querySelector('participantes');
-let listaDeAgregados = document.querySelector('result-list');
-let maximoDeIntegrantes = 10;
-
-//console.log(listaDeParticipantes);
-
-// definimos la funciones que estan en los botones de las etiquetas del HTML
-
-//Función para agregar un amigo al arreglo
-function agregarAmigo() {
-    //Hacemos uso de la caja de texto del input con el id amigo
-    let inputAmigo = document.getElementById("amigo");
-
-    // Guardar el valor escrito y eliminar espacios al inicio y final
-    let nombre = inputAmigo.value.trim();
-
-    // Comparamos que la caja de texto no este vacia
-    if (nombre === "") {
-        alert("Ingresa un nombre, no puede ir vacio");
-        return; // Si está vacío, no continuamos
-    }
-
-    //Guardamos el nombre en el arreglo 
-    listaDeAmigos.push(nombre);
-
-    //mandamos a llamar la funcion de lista que actualiza el html para mostrar la lista
-    mostrarLista();
-
-    // Limpiar el campo de texto para el siguiente nombre
-    inputAmigo.value = "";
+function ingresaUnAmigo(){
+    let nombres = document.getElementById('amigo').value;
+    if(nombres===""){
+    alert("Debes escribir un nombre");
+    return;
 }
+    if(listaDeAmigos.includes(nombres)){
+    alert("Este nombre ya fue ingresado, elija otro por favor");
+    return;
+    }
+    listaDeAmigos.push(nombres);
+   
+    verLista();
+    limpiarBanner();
+   
+
+}
+
+ 
+
+function verLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+    listaDeAmigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+        })
+
+}
+
+function limpiarBanner(){
+    let valorCaja = document.querySelector('#amigo').value='';}
+
+function sortearAmigo(){
+    let sorteo=document.getElementById("resultado");
+    let lista = document.getElementById("listaAmigos");
+    let indiceAleatorio=Math.floor(Math.random()*listaDeAmigos.length);
+    let nombreAleatorio= listaDeAmigos[indiceAleatorio];
+    lista.innerHTML="";
+    sorteo.textContent = (`El nombre del amigo secreto es : ${nombreAleatorio}`);
+    listaDeAmigos.length = 0;
+    
+}    
 
 
 
